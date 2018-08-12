8ad217d1bf8b14d5c988f3a405a47515e900ab79
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-08-12 11:26:23 +0200

    Updates on App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      message: 'HELLO TODO APP',
+      message: 'REACT TODO APP',
       newTodo: '',
       todos: []
     };
@@ -31,17 +31,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h3>{this.state.message}</h3>
-        <form onSubmit={(event) => this.formSubmitted(event)}>
-          <label htmlFor="newTodo">NEW TODO</label>
-          <input onChange={(event) => this.newTodoChanged(event)} id="newTodo" name="newTodo"/>
-          <button type="submit">ADD TODO</button>
+        <div className="container">
+        <div className="row">
+         <h3 className="is-size-1 has-text-centered"><i className="fab fa-react fa-spin fa-lg"></i>&nbsp;{this.state.message}</h3>
+        </div>
+          <form onSubmit={(event) => this.formSubmitted(event)}>
+        <div className="field">
+          <label className="label is-medium" htmlFor="newTodo">NEW TODO</label>
+          <div className="control has-icons-left">
+            <input className="input is-large" onChange={(event) => this.newTodoChanged(event)} id="newTodo" name="newTodo"/>
+            <span className="icon is-large is-left">
+              <i className="fas fa-sign-in-alt fa-lg"></i>
+            </span>
+          </div>
+        </div>
+        <div className="row">
+          <button className="button is-medium is-dark" type="submit">ADD TODO</button>
+        </div>
         </form>
+        <br/>
         <ul>
+          <div className="row">
           {this.state.todos.map(todo => {
-            return <li key={todo.title}>{todo.title}</li>
+            return <li className="message is-large" key={todo.title}>{todo.title} <button className="button is-pulled-right">ERASE</button></li>
           })}
+          </div>
         </ul>
+        </div>
       </div>
     );
   }
